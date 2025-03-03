9edfae0a02a955146fb1ea651653164edc2be94e
masonarmand <masonarmand1@gmail.com>
2025-03-03 18:04:28 -0500

    program outline

--- config.h
+++ config.h
@@ -3,8 +3,11 @@
  */
 #define BORDER_FOCUSED 0x55aaaa
 #define BORDER_UNFOCUSED 0xeaffff
-#define BG_COLOR 0x000000;
-#define FG_COLOR 0xffffff;
+#define BG_COLOR 0x000000
+#define FG_COLOR 0xffffff
+
+/* font */
+#define FONT "-misc-fixed-bold-r-normal--13-120-75-75-C-70-iso10646-1"
 
 /*
  * The MODKEY is the modifier key used for keybinds.
@@ -13,10 +16,11 @@
  */
 #define MODKEY Mod4Mask
 
-static const Dael_Keybinding keys[] = {
+static const Dael_Keybinding config_keys[] = {
         /* Modifier | Key | function | args */
         { MODKEY, XK_d, launch_program, "dmenu_run" },
         { MODKEY, XK_Return, launch_program, "st" },
+        { MODKEY | ShiftMask, XK_e, quit, "" },
 
 
         { NoSymbol, 0, 0, 0 } /* end of keybind list (Do not remove) */
--- dael.c
+++ dael.c
@@ -7,25 +7,40 @@
  */
 #include <X11/Xlib.h>
 #include <X11/keysym.h>
+#include <X11/cursorfont.h>
+#include <X11/Xutil.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdbool.h>
 
 typedef struct {
-        unsigned int temp;
-} Dael_Config;
+        Cursor normal;
+        Cursor resize;
+        Cursor grab;
+        Cursor plus;
+} Dael_Cursors;
 
-typedef struct {
-        Dael_Config conf;
-        Window root;
-        Display* dpy;
-} Dael_State;
+typedef struct Dael_Client Dael_Client;
+typedef struct Dael_Workspace Dael_Workspace;
 
-typedef struct Dael_Client {
+struct Dael_Client {
         Window win;
-        struct Dael_Client* prev;
-        struct Dael_Client* next;
-} Dael_Client;
+        int x;
+        int y;
+        int w;
+        int h;
+        bool is_fullscreen;
+        Dael_Client* next;
+        Dael_Client* prev;
+};
+
+struct Dael_Workspace {
+        unsigned int id;
+        Dael_Client* clients;
+        Dael_Workspace* next;
+        Dael_Workspace* prev;
+};
 
 typedef struct {
         unsigned int mod;
@@ -34,30 +49,137 @@ typedef struct {
         const void* arg;
 } Dael_Keybinding;
 
-void grab_keys(Dael_State* state);
+typedef struct {
+        Dael_Cursors curs;
+        Dael_Workspace* workspaces;
+        XFontStruct* font;
+        Window root;
+        Display* dpy;
+        bool running;
+} Dael_State;
+
+typedef struct {
+        int event_type;
+        void (*handler)(XEvent*);
+} Dael_EventHandler;
+
 void launch_program(const char* program);
-void handle_key_press(Dael_State* state, XEvent* e);
+void quit(const char* args);
 
 #include "config.h"
 
 
+void grab_keys();
+void handle_event(XEvent* e);
+void handle_key_press(XEvent* e);
+void Dael_State_init(Dael_State* wm);
+void Dael_State_free(Dael_State* wm);
+int xerror_handler(Display* display, XErrorEvent* error);
+
+/* XEvent handler functions */
+Dael_EventHandler event_handlers[] = {
+    { KeyPress, handle_key_press },
+    { 0, NULL }
+};
+
+/* global window manager state */
+Dael_State wm = { 0 };
+
+
 int main(void)
 {
+        XEvent e;
+
+        XSetErrorHandler(xerror_handler);
+
+        if (!(wm.dpy = XOpenDisplay(NULL))) {
+                fprintf(stderr, "Failed to open display.\n");
+                return 1;
+        }
+
+        wm.root = DefaultRootWindow(wm.dpy);
+
+        XSelectInput(
+                wm.dpy, wm.root,
+                SubstructureRedirectMask |
+                SubstructureNotifyMask |
+                PropertyChangeMask |
+                ButtonPressMask |
+                ButtonReleaseMask |
+                PointerMotionMask
+        );
+
+        grab_keys(&wm);
+
+        XFlush(wm.dpy);
+
+        /* XSetErrorHandler(xerror_handler); */
+
+        XSync(wm.dpy, False);
+        wm.running = true;
+
+        while (wm.running) {
+                XEvent e;
+                XNextEvent(wm.dpy, &e);
+                handle_event(&e);
+        }
+
+        Dael_State_free(&wm);
         return 0;
 }
 
 
-void grab_keys(Dael_State* state)
+void Dael_State_init(Dael_State* wm)
+{
+        if (!(wm->dpy = XOpenDisplay(NULL)))
+                fprintf(stderr, "Failed to open display.\n");
+        wm->root = DefaultRootWindow(wm->dpy);
+
+        XSelectInput(
+                wm->dpy, wm->root,
+                SubstructureRedirectMask |
+                SubstructureNotifyMask |
+                PropertyChangeMask |
+                ButtonPressMask |
+                ButtonReleaseMask |
+                PointerMotionMask
+        );
+
+        wm->curs.normal = XCreateFontCursor(wm->dpy, XC_left_ptr);
+        wm->curs.grab = XCreateFontCursor(wm->dpy, XC_fleur);
+        wm->curs.resize = XCreateFontCursor(wm->dpy, XC_sizing);
+        wm->curs.plus = XCreateFontCursor(wm->dpy, XC_plus);
+        XDefineCursor(wm->dpy, wm->root, wm->curs.normal);
+
+        wm->font = XLoadQueryFont(wm->dpy, FONT);
+        if (!wm->font) {
+                fprintf(stderr, "Failed to load font\n");
+        }
+}
+
+
+void Dael_State_free(Dael_State* wm)
+{
+        XFreeFont(wm->dpy, wm->font);
+        XFreeCursor(wm->dpy, wm->curs.normal);
+        XFreeCursor(wm->dpy, wm->curs.resize);
+        XFreeCursor(wm->dpy, wm->curs.grab);
+        XFreeCursor(wm->dpy, wm->curs.plus);
+        XCloseDisplay(wm->dpy);
+}
+
+
+void grab_keys()
 {
         unsigned int i = 0;
-        XUngrabKey(state->dpy, AnyKey, AnyModifier, state->root);
+        XUngrabKey(wm.dpy, AnyKey, AnyModifier, wm.root);
 
-        while (keys[i].key_sym != NoSymbol) {
-                KeySym key_sym = keys[i].key_sym;
-                KeyCode keycode = XKeysymToKeycode(state->dpy, key_sym);
-                unsigned int mod = keys[i].mod;
+        while (config_keys[i].key_sym != NoSymbol) {
+                KeySym key_sym = config_keys[i].key_sym;
+                KeyCode keycode = XKeysymToKeycode(wm.dpy, key_sym);
+                unsigned int mod = config_keys[i].mod;
 
-                XGrabKey(state->dpy, keycode, mod, state->root, True,  GrabModeAsync, GrabModeAsync);
+                XGrabKey(wm.dpy, keycode, mod, wm.root, True,  GrabModeAsync, GrabModeAsync);
                 i++;
         }
 }
@@ -80,15 +202,47 @@ void launch_program(const char* program)
 }
 
 
-void handle_key_press(Dael_State* state, XEvent* e)
+void quit(const char* args)
 {
-        /*
-        unsigned int i;
-        XKeyEvent* key_ev = &e->xkey;
-        KeySym key_sym = XKeycodeToKeysym(state->dpy, (KeyCode)key_ev->keycode, 0);
+        (void) args;
+        wm.running = false;
+}
 
-        while (keys[i].key_sym != NoSymbol) {
-                if (key_sym == keys[i].key_sym)
+
+void handle_event(XEvent* e)
+{
+        Dael_EventHandler* h;
+        for (h = event_handlers; h->handler; h++) {
+                if (e->type == h->event_type) {
+                        h->handler(e);
+                        return;
+                }
+        }
+}
+
+
+void handle_key_press(XEvent* e)
+{
+        unsigned int i = 0;
+        XKeyEvent k = e->xkey;
+        (void) wm;
+        while (config_keys[i].key_sym != NoSymbol) {
+                if ((k.state & config_keys[i].mod)
+                && (k.keycode == XKeysymToKeycode(wm.dpy, config_keys[i].key_sym))
+                && (config_keys[i].func)) {
+                        config_keys[i].func(config_keys[i].arg);
+
+                }
                 i++;
-        }*/
+        }
+}
+
+
+int xerror_handler(Display* display, XErrorEvent* error)
+{
+        char error_msg[120];
+        XGetErrorText(display, error->error_code, error_msg, sizeof(error_msg));
+        fprintf(stderr, "X Error: %s\n", error_msg);
+        /*exit(1);*/
+        return 0;
 }
